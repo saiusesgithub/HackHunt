a6686f16cdc3a01f0db5f1656af446f219ffa782
saiusesgithub <saisrujantesting@gmail.com>
2025-09-03 18:56:19 +0530

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'HackHunt',
       theme: ThemeData(
+        fontFamily: 'Poppins',
         colorScheme: ColorScheme.fromSeed(
           seedColor: Color.fromARGB(255, 131, 254, 184),
         ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 27, 26, 26),
       ),
       home: Homescreen(),
     );
--- lib/pages/homescreen.dart
+++ lib/pages/homescreen.dart
@@ -10,6 +10,20 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'HackRadar',
+          style: TextStyle(
+            fontFamily: 'Orbitron',
+            fontWeight: FontWeight.w500,
+            fontSize: 25,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 131, 254, 184),
+        foregroundColor: Colors.black,
+      ),
+    );
   }
 }
